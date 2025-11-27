37df7ccb6f18d32ba18f2b2075d5f21dd529fe9c
CiaranMccarthy1 <g00463067@atu.ie>
2025-11-27 19:59:05 +0000

    Added settings page and themes

--- PokemonTeamBuilder/App.xaml.cs
+++ PokemonTeamBuilder/App.xaml.cs
@@ -5,6 +5,30 @@
         public App()
         {
             InitializeComponent();
+
+      
+            LoadTheme();
+        }
+
+        private void LoadTheme()
+        {
+            bool isDarkMode = Preferences.Get("DarkMode", false);
+
+            var mergedDictionaries = Application.Current.Resources.MergedDictionaries;
+
+            if (mergedDictionaries.Count > 0)
+            {
+                mergedDictionaries.Clear();
+            }
+
+            if (isDarkMode)
+            {
+                mergedDictionaries.Add(new Themes.DarkTheme());
+            }
+            else
+            {
+                mergedDictionaries.Add(new Themes.LightTheme());
+            }
         }
 
         protected override Window CreateWindow(IActivationState? activationState)
